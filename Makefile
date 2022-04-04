d929714f66b7087c45e8410e300318529b2b2853
VanAntonietti <v.antonietti@icloud.com>
2022-04-04 14:18:38 -0300

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+NAME = minishell
+
+CC = gcc
+
+CFLAGS = -Wall -Wextra -Werror
+
+LIBFT = ./libft/libft.a
+
+FILES = main.c		\
+		minishell.c	\
+
+SRC=$(addprefix src/, $(FILES))
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(LIBFT)
+	$(CC) $(SRC) $(CFLAGS) -I ./header -L ./libft -lft -g3 -o $(NAME)
+
+$(LIBFT):
+	make others -C ./libft
+
+clean:
+	rm -f $(LIBFT_OBJS) $(NAME) $(NAME_BONUS)
+	make clean -C ./libft
+
+fclean:
+	make fclean -C ./libft
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/include/mini_shell.h
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_shell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 13:29:26 by vantonie          #+#    #+#             */
+/*   Updated: 2022/04/04 13:56:41 by vantonie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef MINI_SHELL_H
+# define MINI_SHELL_H
+
+# include "../libft/libft.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
+
+/*
+readline
+rl_clear_history
+rl_on_new_line
+rl_replace_line
+rl_redisplay
+add_history
+printf
+malloc
+free
+write
+access
+open
+read,
+close
+fork
+wait
+waitpid
+wait3
+wait4
+signal
+sigaction
+sigemptyset
+sigaddset
+kill
+exit
+getcwd
+chdir
+stat
+lstat
+fstat
+unlink
+execve
+dup
+dup2
+pipe
+opendir
+readdir
+closedir
+strerror
+perror
+isatty
+ttyname
+ttyslot
+ioctl
+getenv
+tcsetattr
+tcgetattr
+tgetent
+tgetflag
+tgetnum
+tgetstr
+tgoto
+tputs
+*/
